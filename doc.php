ead44b21ae92ccddbf6a0bf2c81fd9af9324d45c
Md Jamrul Islam <jamrulislamjp@gmail.com>
2026-03-25 13:09:53 +0600

    this 2 package add for test automation

---
+++ doc.php
@@ -0,0 +1,16 @@
+create laravel project -> composer create-project laravel/laravel learn
+run project -> php artisan serve
+run fronend -> npm install && npm run dev
+database change -> .env
+
+github -> git init
+git action -> git add . && git commit -m "first commit"
+
+test automation -> C:\laragon\www\learn\.github\workflows\deploy.yml
+composer require --dev brianium/paratest work with paratest -> vendor\bin\paratest
+--testdox more faster than phpunit -> php artisan test 5x faster
+
+phpstan/phpstan -> code run previous check where is worng code -> vendor\bin\phpstan analyse app --level=7
+
+install larastan -> composer require --dev nunomaduro/larastan
+configer larastan -> phpstan.neon
